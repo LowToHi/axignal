edf191c7e135b465cd72836c4d4a9cddfa82e709
LowToHi <nocodeworker@gmail.com>
2026-07-28 14:56:19 +0200

    Register F1 validation API

--- apps/api/src/axignal_api/application.py
+++ apps/api/src/axignal_api/application.py
@@ -3,10 +3,12 @@ from axignal_api.main import app
 from axignal_api.persistent_document_research import router as document_research_router
 from axignal_api.persistent_research import router as persistent_research_router
 from axignal_api.research import router as prototype_research_router
+from axignal_api.validation import router as validation_router
 
 app.include_router(prototype_research_router)
 app.include_router(document_research_router)
 app.include_router(persistent_research_router)
 app.include_router(human_review_router)
+app.include_router(validation_router)
 
 __all__ = ["app"]
