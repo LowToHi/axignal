ae3ed2837f2928733891b73850fb12c3a1927fc2
LowToHi <nocodeworker@gmail.com>
2026-07-29 17:07:32 +0200

    Register persistent TED ResearchRun route

--- apps/api/src/axignal_api/application.py
+++ apps/api/src/axignal_api/application.py
@@ -2,6 +2,7 @@ from axignal_api.human_review import router as human_review_router
 from axignal_api.main import app
 from axignal_api.persistent_document_research import router as document_research_router
 from axignal_api.persistent_research import router as persistent_research_router
+from axignal_api.persistent_ted_research import router as ted_research_router
 from axignal_api.pilot_health import router as pilot_health_router
 from axignal_api.research import router as prototype_research_router
 from axignal_api.validation import router as validation_router
@@ -10,6 +11,7 @@ app.include_router(pilot_health_router)
 app.include_router(prototype_research_router)
 app.include_router(document_research_router)
 app.include_router(persistent_research_router)
+app.include_router(ted_research_router)
 app.include_router(human_review_router)
 app.include_router(validation_router)
 
