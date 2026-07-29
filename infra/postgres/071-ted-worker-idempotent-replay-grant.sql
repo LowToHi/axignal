899e234094a99607c69ed5277bbd1bf677949296
LowToHi <nocodeworker@gmail.com>
2026-07-29 15:17:52 +0200

    Narrow TED worker replay privilege

--- infra/postgres/071-ted-worker-idempotent-replay-grant.sql
+++ infra/postgres/071-ted-worker-idempotent-replay-grant.sql
@@ -1,27 +1,5 @@
+REVOKE UPDATE ON axignal_global.candidate_claims FROM axignal_ted_worker;
+
 GRANT UPDATE (updated_at)
   ON axignal_global.candidate_claims
   TO axignal_ted_worker;
-
-REVOKE UPDATE (
-  fingerprint,
-  opportunity_id,
-  subject_id,
-  predicate,
-  object_value,
-  statement,
-  kind,
-  state,
-  evidence_ids,
-  producer_type,
-  producer_id,
-  method_version,
-  canonical_claim_id,
-  rejection_reasons,
-  relationship,
-  model_version,
-  prompt_version,
-  extraction_confidence,
-  assumptions,
-  unknowns
-) ON axignal_global.candidate_claims
-FROM axignal_ted_worker;
