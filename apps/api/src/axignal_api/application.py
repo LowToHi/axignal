598431cf13e7758540f069130bbb764ef4cd9d52
LowToHi <nocodeworker@gmail.com>
2026-08-03 22:22:08 +0200

    feat(axent): register customer notification routes

--- apps/api/src/axignal_api/application.py
+++ apps/api/src/axignal_api/application.py
@@ -4,6 +4,7 @@ from axignal_api.axent_consent_routes import router as axent_consent_router
 from axignal_api.axent_material_action_routes import (
     router as axent_material_action_router,
 )
+from axignal_api.axent_notification_routes import router as axent_notification_router
 from axignal_api.axent_routes import router as axent_router
 from axignal_api.billing_read_routes import router as billing_read_router
 from axignal_api.billing_reconciliation_routes import (
@@ -42,6 +43,7 @@ app.include_router(axent_router)
 app.include_router(axent_consent_router)
 app.include_router(axent_action_router)
 app.include_router(axent_material_action_router)
+app.include_router(axent_notification_router)
 app.include_router(axent_admin_router)
 app.include_router(entitlement_router)
 app.include_router(billing_router)
