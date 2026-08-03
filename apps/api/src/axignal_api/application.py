3979717f8f0768302ffdde856ebd8752c049c002
LowToHi <nocodeworker@gmail.com>
2026-08-03 22:16:48 +0200

    feat(axent): register consent preview routes

--- apps/api/src/axignal_api/application.py
+++ apps/api/src/axignal_api/application.py
@@ -1,4 +1,5 @@
 from axignal_api.axent_admin_routes import router as axent_admin_router
+from axignal_api.axent_consent_routes import router as axent_consent_router
 from axignal_api.axent_routes import router as axent_router
 from axignal_api.billing_read_routes import router as billing_read_router
 from axignal_api.billing_reconciliation_routes import (
@@ -34,6 +35,7 @@ app.include_router(persistent_research_router)
 app.include_router(ted_research_router)
 app.include_router(subscriber_workspace_router)
 app.include_router(axent_router)
+app.include_router(axent_consent_router)
 app.include_router(axent_admin_router)
 app.include_router(entitlement_router)
 app.include_router(billing_router)
